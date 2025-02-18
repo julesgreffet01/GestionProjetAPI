645a04ee597c76d6ae3d00fbcc3c94a4b17b2765
Jules Greffet <154525112+julesgreffet01@users.noreply.github.com>
2025-02-18 15:28:47 +0100

    jules

--- src/Routes/Admin/UserAdminRoutes.ts
+++ src/Routes/Admin/UserAdminRoutes.ts
@@ -3,10 +3,7 @@ const router = express.Router();
 import {UserController} from "../../Controllers/UserController";
 import {loginUnique} from "../../Middlewares/LoginUnique";
 
-
-// On place les routes "force" avant la route générique :id
-router.get('/force', UserController.forceGetAll);
-
+router.get('/', UserController.forceGetAll);
 router.get('/findComplet/:id', UserController.findComplet);
 
 // post
