70d3f3f712f3fbe72b001545133edd66621accb7
Jules Greffet <154525112+julesgreffet01@users.noreply.github.com>
2025-02-18 15:25:56 +0100

    jules

--- src/Routes/Admin/AdminRoutes.ts
+++ src/Routes/Admin/AdminRoutes.ts
@@ -7,10 +7,6 @@ import RoleAdminRoutes from "./RoleAdminRoutes";
 
 router.post('/login', AdminController.authentification);
 
-router.get('/token', adminAuth, (req, res) => {
-    res.status(200).json({message: "good token"});
-})
-
 router.use('/user', adminAuth, UserAdminRoutes);    //ok
 router.use('/role', adminAuth, RoleAdminRoutes);    //oks
 
