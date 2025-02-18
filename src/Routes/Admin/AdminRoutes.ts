2422f480ca049442645428a4e513e9210bf7e585
Jules Greffet <154525112+julesgreffet01@users.noreply.github.com>
2025-02-18 15:24:25 +0100

    jules

--- src/Routes/Admin/AdminRoutes.ts
+++ src/Routes/Admin/AdminRoutes.ts
@@ -4,13 +4,6 @@ import {AdminController} from "../../Controllers/AdminController";
 import {adminAuth} from "../../Middlewares/AdminToken";
 import UserAdminRoutes from "./UserAdminRoutes";
 import RoleAdminRoutes from "./RoleAdminRoutes";
-import ProjectAdminRoutes from "./Project/ProjectAdminRoutes";
-import ProjectUserAdminRoutes from "./Project/ProjectUserAdminRoutes";
-import ToDoAdminRoutes from "./ToDo/ToDoAdminRoutes";
-import ToDoTaskUserAdminRoutes from "./ToDo/ToDoTaskUserAdminRoutes";
-import TrelloAdminRoutes from "./Trello/TrelloAdminRoutes";
-import TrelloListAdminRoutes from "./Trello/TrelloListAdminRoutes";
-import TrelloCardUserAdminRoutes from "./Trello/TrelloCardUserAdminRoutes";
 
 router.post('/login', AdminController.authentification);
 
@@ -19,13 +12,6 @@ router.get('/token', adminAuth, (req, res) => {
 })
 
 router.use('/user', adminAuth, UserAdminRoutes);    //ok
-router.use('/role', adminAuth, RoleAdminRoutes);    //ok
-router.use('/project', adminAuth, ProjectAdminRoutes);  //ok
-router.use('/projectUser', adminAuth, ProjectUserAdminRoutes);  //todo a mettre seulement chez les users
-router.use('/toDo', adminAuth, ToDoAdminRoutes);    //todo a mettre seulement chez les users
-router.use('/taskUser', adminAuth, ToDoTaskUserAdminRoutes);    //todo a mettre seulement chez les users
-router.use('/trello', adminAuth, TrelloAdminRoutes);        //todo a mettre seulement chez les users
-router.use('/trelloList', adminAuth, TrelloListAdminRoutes);    //todo a mettre seulement chez les users
-router.use('/cardUser', adminAuth, TrelloCardUserAdminRoutes);  //todo a mettre seulement chez les users
+router.use('/role', adminAuth, RoleAdminRoutes);    //oks
 
 export default router;
