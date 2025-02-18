99dd45d4a036e02206c61e80cce8f920382a44d2
Jules Greffet <154525112+julesgreffet01@users.noreply.github.com>
2025-02-18 15:23:17 +0100

    jules

--- src/Routes/User/UserRoutes.ts
+++ src/Routes/User/UserRoutes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 const router = express.Router();
 import {UserController} from "../../Controllers/UserController";
 import RoleUserRoutes from "./RoleUserRoutes";
+import ProjectRoutes from "./Project/ProjectRoutes";
 
 router.get('/login', UserController.authenticate);
 router.get('/complet/:id', UserController.findComplet);
@@ -10,5 +11,6 @@ router.put('/:id', UserController.update)
 
 
 router.use('/role', RoleUserRoutes);
+router.use('/project', ProjectRoutes);
 
 export default router;
